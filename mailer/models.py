d586ea397aabaa9d607e139763e6b77ea570687a
OBrien McQuade <62272369+McFlyin619@users.noreply.github.com>
2021-03-31 01:01:48 -0700

    final

--- mailer/models.py
+++ mailer/models.py
@@ -1,48 +1,37 @@
-#-*- coding: utf-8 -*-
-
 from __future__ import unicode_literals
 
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from django.db.models import Sum
 
 
 class Company(models.Model):
     name = models.CharField(max_length=150)
     bic = models.CharField(max_length=150, blank=True)
 
-    def get_order_count(self):
-        orders = 0
-        for order in self.orders.all():
-            orders += 1
-        return orders
-
+    @property
     def get_order_sum(self):
-        total_sum = 0
-        for contact in self.contacts.all():
-            for order in contact.orders.all():
-                total_sum += order.total
-        return total_sum
+        return self.company_orders.aggregate(Sum('total'))
 
-
+    @property
+    def get_company_order_count(self):
+        return self.company_orders.select_related('company').count()
+    
 class Contact(models.Model):
-    company = models.ForeignKey(
-        Company, related_name="contacts", on_delete=models.PROTECT)
+    company = models.ForeignKey(Company, related_name="contacts", on_delete=models.PROTECT)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150, blank=True)
     email = models.EmailField()
 
-    def get_order_count(self):
-        orders = 0
-        for order in self.orders.all():
-            orders += 1
-        return orders
-
+    @property
+    def get_contacts_order_count(self):
+        return self.contact_orders.select_related('company').count()
 
 @python_2_unicode_compatible
 class Order(models.Model):
     order_number = models.CharField(max_length=150)
-    company = models.ForeignKey(Company, related_name="orders")
-    contact = models.ForeignKey(Contact, related_name="orders")
+    company = models.ForeignKey(Company, related_name="company_orders")
+    contact = models.ForeignKey(Contact, related_name="contact_orders")
     total = models.DecimalField(max_digits=18, decimal_places=9)
     order_date = models.DateTimeField(null=True, blank=True)
     # for internal use only
@@ -52,3 +41,5 @@ class Order(models.Model):
 
     def __str__(self):
         return "%s" % self.order_number
+
+    
--- mailer/views.py
+++ mailer/views.py
@@ -1,12 +1,10 @@
 #-*- coding: utf-8 -*-
+from django.db.models.query import Prefetch
 from django.shortcuts import render
-# Create your views here.
 from django.views.generic import ListView
-
 from mailer.models import Company
-
-
 class IndexView(ListView):
     template_name = "mailer/index.html"
     model = Company
     paginate_by = 100
+
